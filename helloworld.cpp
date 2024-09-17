29a90d7751f9d11221cb213fa3ea93affa29f320
axel adelwyn <axeladelwyn322@gmail.com>
2024-09-17 19:38:37 +0700

    second commit

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,8 +7,14 @@ using namespace std;
 int main()
 {
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
+    vector<string> testing {"This", "test", "abc"};
     for (const string& word : msg)
+    {
+        cout << word << " ";
+        
+    }
+    cout << endl;
+    for (const string& word: testing)
     {
         cout << word << " ";
     }
